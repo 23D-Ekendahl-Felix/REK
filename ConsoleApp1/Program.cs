2e44cb3482fb7e26a8a1a252c21c4d962f7be8ad
felix e <felix.ekendahl@elev.ga.ntig.se>
2024-10-21 08:51:17 +0200

    japp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿List<string> rekname = ["Felix", "Matija", "Kevin"];
+List<string> inne = [];
+string name;
+Console.WriteLine("Skriv ditt namn:");
+name = Console.ReadLine();
+
+rekname.Add(name);
+
+
+System.Console.WriteLine("\nLista med namn");
+
+
+for (int i = 0; i < rekname.Count; i++)
+{
+    System.Console.WriteLine($"[{i}] {rekname[i]}");
+}
+
+Console.WriteLine("\nChoose a recruit:");
+{
+  int rNum = 0;
+  bool couldConvert = false;
+  while (couldConvert == false || rNum < 0 || rNum >= rekname.Count)
+  {
+    string nText = Console.ReadLine();
+    couldConvert = int.TryParse(nText, out rNum);
+
+    if (couldConvert == false)
+    {
+      Console.WriteLine("That's not a number!");
+    }
+    else if (rNum < 0 || rNum <= rekname.Count)
+    {
+      Console.WriteLine("That's not a valid index!");
+    }
+  }
+
+  string recruit = rekname[rNum];
+  inne.Add(recruit);
+  rekname.RemoveAt(rNum);
+}
+
+Console.WriteLine($"You recruited a total of {inne.Count} soldiers.");
+
+Console.WriteLine("Press ENTER to quit");
+
+
+Console.ReadLine();
